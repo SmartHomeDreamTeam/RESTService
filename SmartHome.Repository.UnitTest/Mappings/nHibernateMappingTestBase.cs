177174f7232fa50f6c860e666c2566205cc3f107
tonymazn <tonymazn@gamil.com>
2014-11-04 10:49:48 -0700

    Make a copy of the NHibernateHelper

---
+++ SmartHome.Repository.UnitTest/Mappings/nHibernateMappingTestBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate;
+using NHibernate.Cfg;
+using NUnit.Framework;
+using SmartHome.Repository.Implementation;
+
+namespace SmartHome.Repository.UnitTest.Mappings
+{
+    public abstract class nHibernateMappingTestBase
+    {
+        private ITransaction tx;
+        private ISession session;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            session = NHibernateHelper.GetCurrentSession();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            NHibernateHelper.CloseSession();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            tx = session.BeginTransaction();
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            tx.Rollback();
+        }
+
+    }
+}
